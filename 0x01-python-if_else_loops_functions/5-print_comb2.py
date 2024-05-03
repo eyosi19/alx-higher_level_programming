94e63ad69cd0ee2ad7f6a621aee42be0c3d5286a
Eyosiyas <deadlyhacker607@gmail.com>
2024-05-03 18:40:57 -0400

    if_else_loops_functions

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
-for i in range(0, 10):
-    for j in range(0 ,10):
-        if (i == 9 and j == 9):
-            print("99")
-            break
-        print("{}{}, ".format(i,j), end="")
+for i in range(0, 100):
+    if (i == 99):
+        print(99)
+        break
+    print("{:02d}, ".format(i), end="")
